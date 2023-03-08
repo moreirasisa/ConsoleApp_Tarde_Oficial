9af53ea37a92861937f8cc0f4642120dd0b31a66
Isadora Moreira <moreirassisadora@gmail.com>
2023-03-08 15:47:30 -0300

    Criei as classes Cliente.cs e Endereco.cs, os construtores e os métodos get e set de cada classe

---
+++ ConsoleApp-Tarde/Cliente.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_Tarde
+{
+    public class Cliente
+    {
+        private int Id;
+        private string Nome;
+        private string Telefone;
+
+        public Cliente(int id, string nome, string telefone)
+        {
+            Id = id;
+            Nome = nome;
+            Telefone = telefone;
+        }
+
+        public int getId()
+        {
+            return Id;
+        }
+        public void setId(int id)
+        {
+            Id = id;
+        }
+
+        public string getNome()
+        {
+            return Nome;
+        }
+        public void setNome(string nome)
+        {
+            Nome = nome;
+        }
+
+        public string getTelefone()
+        {
+            return Telefone;
+        }
+        public void setTelefone(string telefone)
+        {
+            Telefone = telefone;
+        }
+    }
+}
