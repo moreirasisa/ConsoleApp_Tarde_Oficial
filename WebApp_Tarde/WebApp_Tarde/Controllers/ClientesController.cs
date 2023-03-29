11fa10f44101c95905064de4d18c3ce0a3fe9566
ISADORA SILVA <moreirassisadora@gmail.com>
2023-03-29 16:03:14 -0300

    aula 29/03

---
+++ WebApp_Tarde/WebApp_Tarde/Controllers/ClientesController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp_Tarde.Controllers
+{
+    public class ClientesController : Controller
+    {
+        public IActionResult Lista()
+        {
+            return View();
+        }
+
+        public IActionResult Cadastro()
+        {
+            return View();
+        }
+    }
+}
